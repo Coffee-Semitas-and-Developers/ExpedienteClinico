8e689f9c1c7dbb4b15a8baef9272f15690485f62
Jorge Alberto Iglesias <iglesias9627@gmail.com>
2018-12-05 00:10:47 -0600

    arreglo para que funcione app

--- medEvolution/medEvolution/Controllers/MedicamentosController.cs
+++ medEvolution/medEvolution/Controllers/MedicamentosController.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using MedEvolution.Models.App;
+using medEvolution.Models.App;
 
 namespace medEvolution.Controllers
 {
